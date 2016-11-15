52ea07e107bf9257bd6e3d3538c455c17b156e19
kendradalley <kendracdalley@gmail.com>
2016-11-15 11:32:42 -0800

    webpack.config.js updates

--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,31 @@ const assetPath = path.join(projectRoot, 'dist')
 
 
 module.exports = {
+  // tells Webpack where to start looking for source files
   context: projectRoot,
+  // entry point to app
   entry: './src/index.js',
+  // object for how and where Webpack will be writing output files
   output: {
     path: assetPath,
     filename: 'bundle.js',
+    // path for dev server will be outputting a bundle 'in memory'
     publicPath: '/dist/'
   },
+  // tells Webpack about the code it should be processing
   resolve: {
     modules: ['node_modules', 'src'],
     extensions: ['', '.js', '.jsx', '.json']
   },
+  // for logging
   stats: {
     colors: true,
     reasons: true,
     chunks: false
   },
+  // options applied to modules being processed
   module: {
+    // array that will be applied to files in modules, files are tested against a regex and if passed will process those files
     loaders: [
       {
         test: /\.jsx?$/,
